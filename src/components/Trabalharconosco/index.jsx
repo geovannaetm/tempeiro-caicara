8b04c44eb959321a6edf77c59229ce51055fb73a
Melissa Alves <alves.melissa1@aluno.ifsp.edu.br>
2025-09-18 17:39:27 -0300

    criando o trabalho conosco

--- src/components/Trabalharconosco/index.jsx
+++ src/components/Trabalharconosco/index.jsx
@@ -1,25 +1,38 @@
 import styles from './Trabalharconosco.module.css'
-import Image from 'next/image'
 import Link from 'next/link'
-
-
-
+import { IoFishOutline } from "react-icons/io5";
 
 export default function Trabalharconosco() {
-    return(
-        <div className={styles.container}>
+    return (
+        <section className={styles.section}>
+            <div className={styles.container}>
+                {/* Lado Esquerdo */}
+                <div className={styles.left}>
+                    <h2>Deseja trabalhar conosco?</h2>
+                    <p className={styles.subtitle}>
+                        Junte-se ao time de quem valoriza o tempero caiçara!
+                    </p>
+                    <p className={styles.small}>
+                        Se é do Litoral Norte, tem que ter Tempero Caiçara!
+                    </p>
+                    <button className={styles.button}>Clique aqui e cadastre-se!</button>
+                </div>
+                 {/* Lado Direito */}
+                <div className={styles.right}>
+                    <div className={styles.titleWrapper}>
+                        <h3 className={styles.title}>Cadastre-se</h3>
+                        <IoFishOutline className={styles.icon} />
+                    </div>
+                    <label>Email:</label>
+                    <input type="email" placeholder="Digite seu e-mail" />
 
+                    <button className={styles.enter}>Entrar</button>
 
-
-
-
-
-
-        </div>
-
-
-
+                    <p className={styles.login}>
+                        Já possui cadastro? <Link href="#">Login</Link>
+                    </p>
+                </div>
+            </div>
+        </section>
     )
-
-
 }
