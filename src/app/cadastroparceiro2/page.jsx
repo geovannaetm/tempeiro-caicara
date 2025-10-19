e2db02438af4e7ea20661850d922b30ba8aa17a9
geovannaetm <geovanna.t@aluno.ifsp.edu.br>
2025-10-19 12:50:32 -0300

    ajuste de acordo com o banco

--- src/app/cadastroparceiro2/page.jsx
+++ src/app/cadastroparceiro2/page.jsx
@@ -18,39 +18,6 @@ export default function Cadastro() {
       <main className={styles.main}>
 
         <form className={styles.formContainer} onSubmit={handleSubmit}>
-          {/* Coluna 1 - Local */}
-          <div className={styles.formSection}>
-            <h2>Insira o Local:</h2>
-
-            <label>
-              Praia*
-              <select className="custom-select" name="praia" required>
-                <option value="">Selecione</option>
-                <option value="indaiá">Indaiá</option>
-                <option value="Martin">Martin de Sá</option>
-                <option value="outra">Outra</option>
-              </select>
-            </label>
-
-            <label>
-              Nº do Quiosque*
-              <input type="text" name="quiosque" placeholder="exemplo: número 34" required />
-            </label>
-
-            <label>
-              Nome do negócio*
-              <input type="text" name="negocio" placeholder="digite o nome do local" required />
-            </label>
-
-            <span className={styles.aviso}>
-                <FaExclamationCircle size={20} color="#8F2929"/>
-                 Preencha os dados corretamente 
-            </span>
-
-          </div>
-
-          {/* Divisor vertical */}
-          <div className={styles.divider}></div>
 
           {/* Coluna 2 - Dados pessoais */}
           <div className={styles.formSection}>
@@ -71,11 +38,28 @@ export default function Cadastro() {
               <input type="text" name="cpfCnpj" placeholder="digite seu CPF ou CNPJ" required />
             </label>
 
+             <label>
+              Praia*
+              <select className="custom-select" name="praia" required>
+                <option value="">Selecione</option>
+                <option value="indaiá">Indaiá</option>
+                <option value="Martin">Martin de Sá</option>
+                <option value="outra">Outra</option>
+              </select>
+            </label>
+
+            <span className={styles.aviso}>
+                <FaExclamationCircle size={20} color="#8F2929"/>
+                 Preencha os dados corretamente 
+            </span>
+
             <Link href="/parceiroadm" passHref>
             <button type="submit" className={styles.btnPrincipal}>
               Clicar para finalizar
             </button>
             </Link>
+
+            
           </div>
         </form>
 
--- src/components/Topo/index.jsx
+++ src/components/Topo/index.jsx
@@ -2,30 +2,32 @@
 "use client";
 
 import { useState } from "react";
-import styles from './Topo.module.css'
-import Image from 'next/image'
+import styles from "./Topo.module.css";
+import Image from "next/image";
 import { PiBasketFill } from "react-icons/pi";
 import { IoPersonCircleOutline } from "react-icons/io5";
-import Link from 'next/link'
-import Logo from '/public/logo.png'
+import Link from "next/link";
+import Logo from "/public/logo.png";
 import { IoMdArrowDropdown } from "react-icons/io";
 import Carrinho from "../Carrinho";
 
 export default function Topo() {
   const [abrirCarrinho, setAbrirCarrinho] = useState(false);
+  const [abrirDropdown, setAbrirDropdown] = useState(false);
+
 
   return (
     <>
       <div className={styles.header}>
         <div className={styles.topo}>
-          <Link href='/'>
-          <Image className={styles.img_logo} src={Logo} alt="Logo" />
+          <Link href="/">
+            <Image className={styles.img_logo} src={Logo} alt="Logo" />
           </Link>
         </div>
 
         <div className={styles.icons}>
-          <button 
-            className={styles.carrinho} 
+          <button
+            className={styles.carrinho}
             onClick={() => setAbrirCarrinho(true)}
             aria-label="Abrir carrinho"
           >
@@ -39,12 +41,33 @@ export default function Topo() {
 
         <div className={styles.subtopo}>
           <Link href="/">HOME</Link>
-          <div className={styles.icon_dropdown}>
-            <Link href="#cardapio">CARDÁPIO <IoMdArrowDropdown /></Link>
-          </div>
-          <Link href="#parceiros" className={styles.none}>PARCERIAS</Link>
+         <div className={styles.dropdown}>
+  <button 
+    className={styles.dropdownToggle} 
+    onClick={() => setAbrirDropdown(!abrirDropdown)}
+  >
+    CARDÁPIO <IoMdArrowDropdown />
+  </button>
+
+  {abrirDropdown && (
+    <div className={styles.dropdownMenu}>
+      <Link href="/cardapio/prato-feito">Prato Feito</Link>
+      <Link href="/cardapio/camarao">Camarão</Link>
+      <Link href="/cardapio/bebidas">Bebidas</Link>
+      <Link href="/cardapio/peixes">Peixes</Link>
+      <Link href="/cardapio/porcoes">Porções</Link>
+      <Link href="/cardapio/sorvetes">Sorvetes</Link>
+      <Link href="/cardapio/acai">Açaí</Link>
+    </div>
+  )}
+</div>
+          <Link href="#parceiros" className={styles.none}>
+            PARCERIAS
+          </Link>
           <Link href="#contato">SOBRE NÓS</Link>
-          <Link href="#trabalheconosco" className={styles.none}>TRABALHE CONOSCO</Link>
+          <Link href="#trabalheconosco" className={styles.none}>
+            TRABALHE CONOSCO
+          </Link>
           <Link href="#contato">CONTATO</Link>
         </div>
       </div>
